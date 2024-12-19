25d8e536e162b7fc552217e2c03e2f95b3fc3a8d
tomaslefever <66651651+tomaslefever@users.noreply.github.com>
2024-12-19 00:48:36 -0300

    3rd revision

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-  }
+  basePath: '/app',
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
